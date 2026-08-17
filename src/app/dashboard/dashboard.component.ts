6665fe4aba0a46f26e3491b09263da3f2f0052bb
Abdullahi <amohamoud1999@gmail.com>
2026-08-17 21:41:09 +0100

    Table added and all page elements + basic styling

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
 
+interface TableRow {
+  id: number;
+  project: string;
+  task: string;
+  team: string;
+  location: string;
+  yesNo: string;
+}
+
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent {
-
+  table: TableRow[] = [
+    {
+      id: 1,
+      project: 'Monster energy launch',
+      task: 'task',
+      team: 'Sales',
+      location: 'Uxbridge',
+      yesNo: 'No',
+    },
+    {
+      id: 2,
+      project: 'Promo Event',
+      task: 'task2',
+      team: 'Promo',
+      location: 'Uxbridge2',
+      yesNo: 'Yes',
+    },
+    {
+      id: 3,
+      project: 'UFC Fight promo',
+      task: 'task3',
+      team: 'Promo',
+      location: 'Uxbridge3',
+      yesNo: 'No',
+    },
+  ];
 }
