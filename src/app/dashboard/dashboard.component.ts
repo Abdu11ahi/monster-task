de09ed2a213f60a5e4f477c9aa5ff904e9968269
Abdullahi <amohamoud1999@gmail.com>
2026-08-23 08:08:50 +0100

    Header and footer styling + Dummy Data

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -27,7 +27,7 @@ export class DashboardComponent {
     {
       id: 2,
       project: 'Promo Event',
-      task: 'task2',
+      task: 'task',
       team: 'Promo',
       location: 'Uxbridge2',
       yesNo: 'Yes',
@@ -35,10 +35,66 @@ export class DashboardComponent {
     {
       id: 3,
       project: 'UFC Fight promo',
-      task: 'task3',
+      task: 'task',
       team: 'Promo',
       location: 'Uxbridge3',
       yesNo: 'No',
     },
+    {
+      id: 4,
+      project: 'UFC Fight promo',
+      task: 'task',
+      team: 'Promo',
+      location: 'Uxbridge3',
+      yesNo: 'No',
+    },
+    {
+  id: 5,
+  project: 'Monster Truck Tour',
+  task: 'task',
+  team: 'Sales',
+  location: 'Birmingham',
+  yesNo: 'No'
+},
+{
+  id: 6,
+  project: 'Formula 1 Promotion',
+  task: 'task',
+  team: 'Marketing',
+  location: 'Silverstone',
+  yesNo: 'Yes'
+},
+{
+  id: 7,
+  project: 'Extreme Sports Event',
+  task: 'task',
+  team: 'Promo',
+  location: 'Manchester',
+  yesNo: 'No'
+},
+{
+  id: 8,
+  project: 'New Product Launch',
+  task: 'task',
+  team: 'Sales',
+  location: 'Leeds',
+  yesNo: 'Yes'
+},
+{
+  id: 9,
+  project: 'Social Media Campaign',
+  task: 'task',
+  team: 'Marketing',
+  location: 'London',
+  yesNo: 'Yes'
+},
+{
+  id: 10,
+  project: 'Gaming Tournament',
+  task: 'task',
+  team: 'Promo',
+  location: 'Bristol',
+  yesNo: 'No'
+},
   ];
 }
